3e40fac41bdc1c2abd339b2a2bdde51d3af1682f
same-js <40536011+same-js@users.noreply.github.com>
2022-07-15 22:19:36 +0900

    [WIP] run docker compose pull && docker compose up & add .gitignore in data

---
+++ strapi-strapi/after/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '605aa7aa9922c63b795447cd37b9d177'),
+    },
+  },
+});
---
+++ strapi-strapi/after/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cbcff46e-58e9-4d41-9d0b-61176f45ac03'
+};
